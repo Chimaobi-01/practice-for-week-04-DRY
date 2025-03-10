38713df8697b27ad22e5e6ff1c7dcb0bdc3f99a2
Chinaka_Chimaobi <80992410+Chimaobi-01@users.noreply.github.com>
2025-03-10 14:22:49 +0000

    add DRY refactored code

--- practice.js
+++ practice.js
@@ -1,47 +1,33 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  let bigNum = bigOrSmall(num1, num2, "big");
+ 
+  return bigNum * 2
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  let bigNum = bigOrSmall(num1, num2, "big");
+
+  return bigNum / 3
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  let bigNum = bigOrSmall(sum1, sum2, "big");
+  
+  return `I ate ${bigNum} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  let smallDog = bigOrSmall(weight1, weight2)
+
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
 }
 
+function bigOrSmall( value1, value2, indicator ) {
+  if(indicator === "big")
+    return Math.max(value1, value2)
+
+  return Math.min(value1, value2)
+}
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
